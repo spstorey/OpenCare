047b900f4f76aa25cbabbc1b6568a9c5e69da8a2
shaun.storey <shaun.storey@autotrader.co.uk>
2016-06-15 00:20:02 +0100

    Added lombok

--- src/main/java/spssoftware/opencare/resource/OrganisationResource.java
+++ src/main/java/spssoftware/opencare/resource/OrganisationResource.java
@@ -1,8 +1,13 @@
 package spssoftware.opencare.resource;
 
+import lombok.Data;
 import org.springframework.hateoas.ResourceSupport;
 
+@Data
 public class OrganisationResource extends ResourceSupport {
 
-
+    private String name;
+    private String description;
+    private String websiteUrl;
+    private String organisationType;
 }
